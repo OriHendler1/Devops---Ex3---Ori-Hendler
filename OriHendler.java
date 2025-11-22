caaae8db610b8003c822f2cfdc84fdc2ff620fb0
OriHendler1 <ori1601@gmail.com>
2025-11-23 01:14:22 +0200

    change name print

--- OriHendler.java
+++ OriHendler.java
@@ -1,7 +1,7 @@
 public class OriHendler{
 	public static void main(String[] args) {
 		for (int i=1;i<=5; i++) {
-			System.out.println("Ori Hendler :)");
+			System.out.println("Ori Hendler :) :D ");
 		}
 	}
 }
